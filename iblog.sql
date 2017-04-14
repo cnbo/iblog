97a82ac0978ea029ff3b8c2c0af994a95cb5bd0c
cnbo <18163912002@163.com>
2017-04-14 22:09:16 +0800

    update iblog.sql

--- iblog.sql
+++ iblog.sql
@@ -5,19 +5,17 @@ USE iblog;
 DROP TABLE IF EXISTS admin;
 
 CREATE TABLE admin (
-  username      VARCHAR(45)  NOT NULL
+  username      VARCHAR(45)
   COMMENT '用户名',
-  password      VARCHAR(45)  NOT NULL
+  password      VARCHAR(45)
   COMMENT '密码',
-  password_salt VARCHAR(45)  NOT NULL
-  COMMENT '加密密码',
-  introduction  VARCHAR(200) NOT NULL DEFAULT ''
+  introduction  VARCHAR(200) DEFAULT ''
   COMMENT '用户简介',
-  email         VARCHAR(64)  NOT NULL DEFAULT '#',
-  github        VARCHAR(64)  NOT NULL DEFAULT '#',
+  email         VARCHAR(64)  DEFAULT '#',
+  github        VARCHAR(64)  DEFAULT '#',
   resume_md     TEXT COMMENT '简历的md格式文本',
-  --   `resume_html` text COMMENT '简历的html格式文本',
-  avatar        VARCHAR(80)  NOT NULL DEFAULT '/img/avatar.jpg'
+  nickname      VARCHAR(45)  COMMENT '昵称',
+  avatar        VARCHAR(80)  DEFAULT '/img/avatar.jpg'
   COMMENT '头像',
   PRIMARY KEY (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
@@ -26,31 +24,29 @@ DROP TABLE IF EXISTS 'blog';
 
 CREATE TABLE blog (
 	id INT NOT NULL AUTO_INCREMENT COMMENT '主键',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  publish_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
-  category_id INT NOT NULL COMMENT '分类id',
-	title VARCHAR(128) NOT NULL COMMENT '文章标题',
-	blog_md TEXT NOT NULL COMMENT 'blog的md格式文本',
-#   blog_html TEXT NOT NULL COMMENT 'blog的html格式文本',
-	read_times INT(11) NOT NULL COMMENT '阅读次数',
-  comment_times INT(11) NOT NULL COMMENT '评论次数',
-	love_times INT(11) NOT NULL COMMENT '喜欢次数',
-	status TINYINT(1) NOT NULL DEFAULT 0 COMMENT '状态:0,草稿 1,发布 2,删除',
+	create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  publish_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
+  category_id INT COMMENT '分类id',
+	title VARCHAR(128) COMMENT '文章标题',
+	blog_md TEXT COMMENT 'blog的md格式文本',
+	read_times INT(11) COMMENT '阅读次数',
+  comment_times INT(11) COMMENT '评论次数',
+	love_times INT(11) COMMENT '喜欢次数',
+	status TINYINT(1) DEFAULT 0 COMMENT '状态:0,草稿 1,发布 2,删除',
 	PRIMARY KEY (id)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户blog表';
 
 DROP TABLE IF EXISTS blog_category;
 
 CREATE TABLE blog_category (
-  id            INT         NOT NULL AUTO_INCREMENT
+  id            INT         AUTO_INCREMENT
   COMMENT '主键',
-  create_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+  create_time   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间',
-  update_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  update_time   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   COMMENT '更新时间',
-  category_name VARCHAR(20) NOT NULL
-  COMMENT 'blog类名',
+  category_name VARCHAR(20) COMMENT 'blog类名',
   PRIMARY KEY (id)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='blog分类表';
 
@@ -58,19 +54,19 @@ CREATE TABLE blog_category (
 DROP TABLE IF EXISTS web_visitor;
 
 CREATE TABLE web_visitor (
-  id            INT         NOT NULL AUTO_INCREMENT
+  id            INT         AUTO_INCREMENT
   COMMENT '主键',
-  create_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+  create_time   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间',
-  update_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  update_time   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   COMMENT '更新时间',
-  visitor_name  VARCHAR(45) NOT NULL
+  visitor_name  VARCHAR(45)
   COMMENT '访客名',
-  password      VARCHAR(45) NOT NULL
+  password      VARCHAR(45)
   COMMENT '密码',
-  password_salt VARCHAR(45) NOT NULL
+  password_salt VARCHAR(45)
   COMMENT '加密密码',
-  avatar        VARCHAR(80) NOT NULL DEFAULT '/img/visitor.jpg'
+  avatar        VARCHAR(80) DEFAULT '/img/visitor.jpg'
   COMMENT '头像',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='网络访客表';
@@ -78,15 +74,15 @@ CREATE TABLE web_visitor (
 DROP TABLE IF EXISTS friend_link;
 
 CREATE TABLE friend_link (
-  id          INT         NOT NULL AUTO_INCREMENT
+  id          INT         AUTO_INCREMENT
   COMMENT '主键',
-  create_time TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+  create_time TIMESTAMP   DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间',
-  update_time TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  update_time TIMESTAMP   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   COMMENT '更新时间',
-  friend_name VARCHAR(45) NOT NULL
+  friend_name VARCHAR(45)
   COMMENT '朋友名',
-  url         VARCHAR(64) NOT NULL DEFAULT '#'
+  url         VARCHAR(64) DEFAULT '#'
   COMMENT '友情链接',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='友情链接表';
@@ -94,15 +90,15 @@ CREATE TABLE friend_link (
 DROP TABLE IF EXISTS blog_love;
 
 CREATE TABLE blog_love (
-  id             INT       NOT NULL AUTO_INCREMENT
+  id             INT       AUTO_INCREMENT
   COMMENT '主键',
-  create_time    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  create_time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间',
-  update_time    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  update_time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   COMMENT '更新时间',
-  blog_id        INT       NOT NULL
+  blog_id        INT
   COMMENT 'blogID',
-  web_visitor_id INT       NOT NULL
+  web_visitor_id INT
   COMMENT '访客ID',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='blog喜欢表';
@@ -110,13 +106,13 @@ CREATE TABLE blog_love (
 DROP TABLE IF EXISTS blog_comment;
 
 CREATE TABLE blog_comment (
-	id INT NOT NULL AUTO_INCREMENT COMMENT '主键',
-	create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-	web_visitor_id INT NOT NULL COMMENT '访客id',
-	blog_id INT NOT NULL COMMENT '文章ID',
-	comment VARCHAR(2000) NOT NULL COMMENT '评论内容',
-	reply_comment_id INT(11) NULL COMMENT '父级评论',
-  parents_comment_id VARCHAR(128) NOT NULL DEFAULT '0,' COMMENT '父级评论路径',
+	id INT AUTO_INCREMENT COMMENT '主键',
+	create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+	web_visitor_id INT COMMENT '访客id',
+	blog_id INT COMMENT '文章ID',
+	comment VARCHAR(2000) COMMENT '评论内容',
+	reply_comment_id INT(11) COMMENT '父级评论',
+  parents_comment_id VARCHAR(128) DEFAULT '0,' COMMENT '父级评论路径',
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='blog评论表';
